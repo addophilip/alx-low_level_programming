7f24af7edde60070587784cbf92fd8f96030123a
addophilip <addphilip027@icloud.com>
2022-09-14 10:46:12 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "master.h"
 
 /**
  * main - main entry
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLIDED
+
+int _putchar(char);
+clear
+[A0x02. C - Functions, nested loops clear
+[A[B:q
