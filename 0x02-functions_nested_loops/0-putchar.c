52ec628af8b6639870d13f1878738c51eb34ec38
addophilip <addphilip027@icloud.com>
2022-09-15 11:18:30 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "master.h"
+#include"main.h"
 
 /**
- * master - master entry
- * Return: int
-*/
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar(10);
+	char str[] = "_putchar";
+	int ch;
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
-}
+}	
