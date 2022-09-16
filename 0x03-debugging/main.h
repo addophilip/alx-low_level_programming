f2c569fb34b41b18e701cb786bec019c79fb5339
addophilip <addphilip027@icloud.com>
2022-09-15 17:17:33 -0700

    done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,14 @@
-#include "main.h"
-/**
- * main - tests function that prints if int is +ve/-ve
- *
- * Return: 0
- */
+#ifndef MAIN_H
+#define MAIN_H
 
-int main(void)
-{
-	int i;
+#include <stdio.h>
 
-	i = 0;
-	positive_or_negative(i);
+void positive_or_negative(int i);
 
-	return (0);
-}
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
