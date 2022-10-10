914cb7fc18ef793458ce624e740d1f01d3e7ccac
addophilip <addophilip027@icloud.com>
2022-10-10 15:59:13 -0700

    Dog work

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef STRUCTS
+#define STRUCTS
 /**
- * struct dog - structure definition of a dog
- * @name: character string
- * @age: integer
- * @owner: character string
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
  */
 typedef struct dog
 {
@@ -13,10 +14,12 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
