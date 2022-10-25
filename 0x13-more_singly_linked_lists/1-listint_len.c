eddc662e5c298ad135f1bf2522729d22531d9dec
ojoyesamson@gmail.com <ojoyesamson@gmail.com>
2022-10-25 02:09:35 -0700

    List on node

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,20 @@
 #include "lists.h"
+
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: pointer to the 1st node of the linked list
- * Return: number of nodes
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodenum = 0;
+	size_t nnodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodenum++;
 		h = h->next;
+		nnodes++;
 	}
-	return (nodenum);
+	return (nnodes);
 }
